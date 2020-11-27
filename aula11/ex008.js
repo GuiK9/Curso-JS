c72cae479b2d34b6c70c6be9d34ad0db6ca94d9c
Guilherme Sousa Ribeiro <74858101+GuiK9@users.noreply.github.com>
2020-11-27 15:29:30 -0300

    Feito antes de ter um git-HUB

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 30.5
+console.log(`a velocidade do carro e ${vel}km/h`)
+if ( vel>60){
+    console.log(`voce ultrapassou a velocidade permitida boa sorte`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`vivendo em ${pais}`)
+if (pais != 'brasil' ){
+    console.log ('mano tu e baiano?')
+}else {
+    console.log('mano ctz tu n e baiano')
+}
